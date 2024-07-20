ae7f084bce0b2bc40185f4218309bf34f486cdae
PanXXHH <987384390@qq.com>
2024-07-20 09:20:23 +0800

    #updated240720002

--- Document.py
+++ Document.py
@@ -2,9 +2,16 @@
 class Document:
     def __init__(self) -> None:
         self.content = ""
-    
-    def append(self,content:str):
+
+    def append(self, content: str):
         self.content += content
-    
-    def export(self,path:str):
-        ...
+
+    def export(self, path: str):
+        if not path:
+            raise Exception("请传入有效的 path 参数！")
+
+        try:
+            with open(path, 'w', encoding='utf-8') as file:
+                file.write(self.content)
+        except Exception as e:
+            print(f"无法写入文件：{path}。错误：{e}")
--- __init__.py
+++ __init__.py
@@ -5,10 +5,17 @@ import Document
 
 
 class PyJ2Md:
-    def __init__(self, template: str, data: Any = None, doc: Document.Document | None = None):
-        self.template = template
+    def __init__(self, data: Any = None, env: Environment | None = None):
         self.data = data
-        self.doc = doc or Document.Document()
+        self.env = env or Environment(
+            block_start_string='<%',
+            block_end_string='%>',
+            variable_start_string='<<',
+            variable_end_string='>>',
+            comment_start_string='<#',
+            comment_end_string='#>'
+        )
+
         # doc要接收传入和公开，目前对整个系统不是特别熟悉，否则会扼杀创新与发现
         self.extensions: list[hotconfig.HotConfig] = []
         self.CONFIG = {
@@ -20,33 +27,27 @@ class PyJ2Md:
             raise TypeError("请传入HotConfig类型的扩展")
         self.extensions.append(extension)
 
-    def render_template(self, template, env: Environment | None = None):
-        if env is None:
-            self.__env = Environment(
-                block_start_string='<%',
-                block_end_string='%>',
-                variable_start_string='<<',
-                variable_end_string='>>',
-                comment_start_string='<#',
-                comment_end_string='#>'
-            )
+    def render_template(self, template) -> Document.Document:
+
+        doc = Document.Document()
 
         for extension in self.extensions:
-            self.doc, self.data, template = extension.pre_render_template(
-                self.__env, self.doc, self.data, template)
+            doc, self.data, template = extension.pre_render_template(
+                self.env, doc, self.data, template)
 
         # 使用环境来渲染模板
-        rendered = self.__env.from_string(template).render(
+        rendered = self.env.from_string(template).render(
             data=self.data, CONFIG=self.CONFIG)
 
-        self.doc.append(rendered)
+        doc.append(rendered)
+        return doc
 
-    def generate_md(self, output_path: str | None = None):
+    # def generate_md(self, output_path: str | None = None):
 
-        # 如果提供了输出路径，则将渲染的内容写入文件
-        if output_path:
-            try:
-                with open(output_path, 'w', encoding='utf-8') as file:
-                    file.write(self.doc.content)
-            except Exception as e:
-                print(f"无法写入文件：{output_path}。错误：{e}")
+    #     # 如果提供了输出路径，则将渲染的内容写入文件
+    #     if output_path:
+    #         try:
+    #             with open(output_path, 'w', encoding='utf-8') as file:
+    #                 file.write(self.doc.content)
+    #         except Exception as e:
+    #             print(f"无法写入文件：{output_path}。错误：{e}")
