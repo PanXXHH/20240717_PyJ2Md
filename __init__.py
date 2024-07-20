ebf411b47fcb2474dc67c483ec32260791361baf
PanXXHH <987384390@qq.com>
2024-07-20 09:04:02 +0800

    #update2024-07-20-001

--- __init__.py
+++ __init__.py
@@ -1,64 +1,24 @@
-# import os
 from typing import Any
-# from pylatex import Document, Command, NoEscape
 from jinja2 import Environment
 import hotconfig
 import Document
 
-import os
-
 
 class PyJ2Md:
-    def __init__(self, template: str, doc: Document.Document = None, data: Any | None = None):
-        self.doc = doc or Document.Document()
+    def __init__(self, template: str, data: Any = None, doc: Document.Document | None = None):
         self.template = template
         self.data = data
+        self.doc = doc or Document.Document()
         # doc要接收传入和公开，目前对整个系统不是特别熟悉，否则会扼杀创新与发现
         self.extensions: list[hotconfig.HotConfig] = []
         self.CONFIG = {
             # 'SUPPORT_CHINESE_LIST': False,
-            # 'IN_WRITING': True
         }
-
-    # def __getattr__(self, name:str):
-    #     if name.startswith('__'):
-    #         return None
-    #     # if name.startswith('_') and name in self.attributes:
-    #     #     return self.attributes[name]
-    #     else:
-    #         return super().__getattribute__(name)
-
-    # def __setattr__(self, name, value):
-    #     print("__setattr__",name, value)
-    #     # 检查属性是否应该是私有的且未通过 name mangling
-    #     if name.startswith('__') and len(name) > 2:
-    #         self.attributes.append((name[2:], value))
-    #     else:
-    #         super().__setattr__(name, value)
-
-    # def add_easy_preamble(self, key: str, value: str, options: str | None = None):
-    #     if options is None:
-    #         self.doc.preamble.append(Command(key, value))
-    #     else:
-    #         self.doc.preamble.append(Command(key, value, options))
 
     def add_extension(self, extension: hotconfig.HotConfig):
         if not isinstance(extension, hotconfig.HotConfig):
             raise TypeError("请传入HotConfig类型的扩展")
         self.extensions.append(extension)
-
-    # def __add_preamble(self):
-    #     # print(self.attributes)
-    #     for name, value in self.attributes:
-    #         if not isinstance(name, str):
-    #             raise TypeError("attributes的键类型必须是str")
-
-    #         if isinstance(value, str):
-    #             # print(name[2:])
-    #             self.doc.preamble.append(Command(name[2:], value))
-    #         else:
-    #             print(value)
-    #             raise TypeError("attributes的值是未知类型：%s" % type(value))
 
     def render_template(self, template, env: Environment | None = None):
         if env is None:
@@ -90,17 +50,3 @@ class PyJ2Md:
                     file.write(self.doc.content)
             except Exception as e:
                 print(f"无法写入文件：{output_path}。错误：{e}")
-
-        # try:
-        #     self.doc.generate_pdf(
-        #         base_name, clean_tex=False, compiler='xelatex')
-        # except Exception as e:
-        #     input(e)
-
-        # os.startfile(os.path.join(
-        #     os.path.dirname(file_path), base_name + '.pdf'))
-
-    # def generate_pdf(self, file_path):
-# test #test
-    # def set_config(self, config):
-    #     self.CONFIG.update(config)
